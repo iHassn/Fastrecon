c231e31bfdfdf8afb12a12ad3c9283e0ed31910c
Hassn <79095544+iHassn@users.noreply.github.com>
2023-02-15 07:23:07 -0500

    Add files via upload

---
+++ Fastrecon.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+echo -e "\e[96m  	      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣄⠴⠔⠂⠀⠀⠀⠀⠀⠐⠂⠦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⢶⠻⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢳⣗⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢠⣼⣛⠮⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠾⣵⢧⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣞⣟⣶⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢫⢿⣝⡧⣄⠀⠀⠀⠀
+⠀⠀⠀⣰⣻⡼⣾⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⣞⣽⣳⢆⠀⠀⠀
+⠀⠀⣸⢷⣫⢷⢯⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⣼⣫⢿⣅⠀⠀
+⠀⢸⣯⢯⡽⣞⣯⡇⠀⠀⠀⠀⠀⢀⣀⣠⣤⢦⣴⢶⣯⣛⣻⣞⣄⠀⠀⠀⠀⠀⠀⢸⡿⣵⢯⣟⡾⣆⠀
+⢀⡿⣞⣯⣽⣛⡶⣇⠀⠀⠀⠀⠀⠀⠈⢉⣾⢿⣽⣻⣞⣷⡻⠊⠛⠂⠀⠀⠀⠀⠀⣸⢿⣽⣻⣞⣿⣽⠀
+⢸⣿⡽⣞⣷⣯⢿⣽⡀⠀⠀⠀⠀⠀⢠⠿⢯⡿⣞⣷⣻⡼⡇⠀⠀⠀⠀⠀⠀⠀⢀⣿⣻⣞⣷⢿⡾⣽⡇
+⢸⣷⢿⣯⣿⣾⣻⣞⡷⡄⠀⠀⠀⠀⠀⣠⣾⢿⣽⣳⣏⣷⣻⣇⠀⠀⠀⠀⠀⣀⣿⢯⣷⢿⣿⣯⣟⣯⡇
+⢸⣿⣻⢻⣽⡾⣷⢯⡿⣷⣦⡀⠀⠀⣸⡿⣽⣻⡾⣗⣯⢾⡵⣯⢧⠀⠀⢀⣴⣻⣽⢿⣾⣻⣾⡟⣯⣟⡇
+⠘⣿⠇⣾⢯⣿⣽⣻⣽⣷⣻⣟⣶⣴⣿⣻⣽⢷⣟⡿⣞⣯⢿⣽⣻⣤⢾⣻⣾⡽⣯⡿⣞⣷⣯⡇⠸⣿⠃
+⠀⠻⠀⣿⣿⣳⣯⣟⣾⡷⣟⣾⣽⡏⢷⡿⣽⣻⡾⣿⡽⣾⣟⣾⡯⢻⣿⡽⣞⣿⣷⣿⣻⢷⣯⡧⠀⠟⠀
+⠀⠀⠀⢹⣷⣯⣷⡟⣿⣟⣯⢿⣾⠁⠘⣿⣯⣷⢿⣳⡿⣷⢯⡿⠁⢸⣷⢿⣻⢷⡏⢷⣿⣻⣷⡇⠀⠀⠀
+⠀⠀⠀⠘⣿⣾⣿⠁⣿⣻⡾⣿⣽⠀⠀⢽⣷⣯⡿⣯⢿⣽⢿⡇⠀⠘⣯⡿⣯⣿⣻⠈⣿⣟⡿⠁⠀⠀⠀
+⠀⠀⠀⠀⠈⢿⡯⠀⢹⣿⣽⣷⣿⡀⠀⠈⣿⣷⣿⣟⣯⡿⡿⠀⠀⢘⣿⣽⣷⣿⡇⠀⣽⡿⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠓⠀⠀⠻⣿⣿⣾⡇⠀⠀⢸⣿⣿⣯⣿⢿⡇⠀⠀⢸⣿⣿⣾⠏⠀⠀⠛⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⠀⠀⠀⣿⣿⣿⣿⡿⠀⠀⠀⣿⣿⡷⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠆⠀⠀⠸⣿⣿⣿⡃⠀⠀⠰⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                        by @h3x1L
+                                        Version 1.0
+                                                      	\e[0m"
+domain=$1
+if [[ -z $domain ]]; then
+	echo -e "Usage: ./FastRecon.sh <domain.com>"
+	exit 1
+fi
+	
+
+	# Check whether tools are exists or not
+   		if [ ! -x "$(command -v subfinder)" ]; then
+   		    echo "[-] subfinder required to run script"
+   		    exit 1
+   		fi
+
+   		if [ ! -x "$(command -v httpx)" ]; then
+   		    echo "[-] httpx required to run script"
+   		    exit 1
+   		fi    
+
+   		if [ ! -x "$(command -v waybackurls)" ]; then
+   		    echo "[-] waybackurls required to run script"
+   		    exit 1
+   		fi
+
+
+			mkdir $domain
+
+		echo -e "\e[93mEnumerating subdomains with SubFinder...\e[0m"
+		subfinder -d $domain -silent > $domain/all-domains.txt
+		echo $domain >> $domain/all-domains.txt
+		echo -e "[- Task Done [✔] -] \e[92mAll domains of $domain are saved in all-domains.txt\e[0m"
+
+ 		sleep 1 # wait for 1 second
+
+		echo "	"
+		echo -e "\e[93mChecking for Live domains, this may take some time.\e[0m"
+		cat $domain/all-domains.txt | sort -u | httpx -silent | sed 's/https\?:\/\///' > $domain/live-domains.txt #tr -d ":443" 
+		echo -e "[- Task Done [✔] -] \e[92mResults are saved in live-domains.txt\e[0m"
+	
+		echo "	"
+		echo -e "\e[93mFetching all URLs/links of $domain, this may take some time..\e[0m"
+		cat $domain/live-domains.txt | waybackurls | sort -u > $domain/urls.txt
+		echo -e "[- Task Done [✔] -] \e[92mAll urls/links of $domain are saved in urls.txt\e[0m"
+	
+		echo "	"
+		echo -e "\e[93mEnumerating all JS files from $domain, this may take some time\e[0m"
+		cat $domain/urls.txt | grep -aEi '\.(js)' | grep -ivE '\.json' | uniq | sort | httpx -silent -mc 200 > $domain/js_files.txt
+		echo -e "[- Task Done [✔] -] \e[92mJs files of $domain are saved in Js_files.txt \e[0m"
+		
+		echo "	"
+		echo -e "\e[93mAll possible Prameters of $domain, this may take some time\e[0m"
+		cat $domain/urls.txt| grep '?*='| sort -u | grep -v 'png\|jpg\|jpeg\|gif\|svg\|ttf\|swf\|css\|eot\|woff\|woff2\|piwik_campaign\|?campaignid=\|ico\|?utm_medium\|?utm_campaign\|?utm_content\|?utm_source\|?utm_term\|?utm_profileid' > $domain/params1.txt
+		cat $domain/params1.txt | awk -F'=' -vOFS='=' '{$NF="FUZZ"}1;' | sort -u > $domain/params.txt 
+		rm $domain/params1.txt 		
+		echo -e "[- Task Done [✔] -] \e[92mPossible Prameters of $domain are saved in params.txt\e[0m"
+
+		echo "	"
+		echo -e "\e[95m~ All files are saved in $domain directory\e[0m"
+		echo -e "\e[5m			Happy Hacking :)\e[0m"
